f2c5c31c604bca00de8ac6ed96bf964ee23ed888
Rafael Costa <rafael.txct@gmail.com>
2023-03-14 10:51:15 -0300

    modifications

--- backup.sh
+++ backup.sh
@@ -1,11 +1,11 @@
 !/bin/bash
 
-find /backup/* -mmin +354 -exec rm -rf {} \; # Set timer to delete old files
+find /BackupDirectory/* -mmin +354 -exec rm -rf {} \;    # Set timer to delete old files
 
-TIME=`date +%I-%d-%b`                  	   # This Command will read the date.
-FILENAME=projects-$TIME.tar.gz             # The filename including the date.
-SRCDIR=/home/rtxct/Desktop/projects/       # Source backup folder.
-DESDIR=/backup                             # Destination of backup file.
-tar -cpzf $DESDIR/$FILENAME $SRCDIR        # Execute backcup
+TIME=`date +%I-%d-%b`                  	      # This Command will read the date.
+FILENAME=projects-$TIME.tar.gz             	# The filename including the date.
+SRCDIR=/DirectoryToBackup       			# Source backup folder.
+DESDIR=/SendBackup                             	# Destination of backup file.
+tar -cpzf $DESDIR/$FILENAME $SRCDIR        	# Execute backcup
 
 
