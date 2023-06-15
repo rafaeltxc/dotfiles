ea0b0b94e5f06f2474c8a638ccc26e56383252e9
Rafael Costa <rafael.txct@gmail.com>
2023-06-15 15:33:45 -0300

    updating system

---
+++ polybar/launch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Launch Polybar, using default config location ~/.config/polybar/config.ini
+polybar main 2>&1 | tee -a /tmp/polybar.log & disown
