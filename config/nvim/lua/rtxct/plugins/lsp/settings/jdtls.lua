b3421fce59e1e923eef4fcf3c1c0ef3ae3b181f6
Rafael Costa <rafael.txct@gmail.com>
2024-03-26 12:10:45 -0300

    Nvim update with jdtls lombok variable

--- config/nvim/lua/rtxct/plugins/lsp/settings/jdtls.lua
+++ config/nvim/lua/rtxct/plugins/lsp/settings/jdtls.lua
@@ -1,12 +1,6 @@
 return {
-	cmd = { "/home/rtxct/.local/share/nvim/mason/bin/jdtls" },
-	init_options = {
-		workspace = true,
-		bundles = {
-			vim.fn.glob("~/.m2/repository/org/projectlombok/lombok/1.18.30/lombok-1.18.30.jar"),
-		},
-		extendedClientCapabilities = {
-			classFileContentsSupport = true,
-		},
-	},
+	cmd = {
+    "jdtls",
+    "--jvm-arg=" .. string.format("-javaagent:%s", vim.fn.expand "$MASON/share/jdtls/lombok.jar"),
+  },
 }
