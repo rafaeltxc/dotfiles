37b7cd9712f5cd6f17f577b0d4ea277a95d906b2
Rafael Costa <rafael.costa@duxus.com.br>
2025-06-19 18:53:00 -0300

    Kitty and adjusts

--- config/zsh/core/options.zsh
+++ config/zsh/core/options.zsh
@@ -1,13 +1,19 @@
 # History
+HISTFILE=${HISTFILE:-$HOME/.zsh_history}
 HISTSIZE=5000
 HISTFILE=~/.zsh_history
 SAVEHIST=$HISTSIZE
 HISTDUP=erase
 
-setopt appendhistory
-setopt sharehistory
-setopt hist_ignore_space
-setopt hist_ignore_all_dups
-setopt hist_save_no_dups
-setopt hist_ignore_dups
-setopt hist_find_no_dups
+setopt HIST_EXPIRE_DUPS_FIRST # Expire a duplicate event first when trimming history
+setopt HIST_IGNORE_SPACE      # Ignore blank spaces on history
+
+setopt APPENDHISTORY
+setopt EXTENDED_HISTORY       # Write the history file in the ':start:elapsed;command' format
+setopt INC_APPEND_HISTORY     # Write to the history file immediately, not when the shell exits
+setopt SHARE_HISTORY          # Share history between all sessions
+
+setopt HIST_IGNORE_DUPS       # Do not record an event that was just recorded again
+setopt HIST_IGNORE_ALL_DUPS   # Delete an old recorded event if a new event is a duplicate
+setopt HIST_FIND_NO_DUPS      # Do not let zsh search for duplicates
+setopt HIST_SAVE_NO_DUPS      # Do not save duplicates in the history
--- config/zsh/misc/customs.zsh
+++ config/zsh/misc/customs.zsh
@@ -8,6 +8,21 @@ zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
 eval "$(fzf --zsh)"
 export FZF_DEFAULT_OPTS='--color=bg+:-1,bg:-1,border:#FFFFFF,spinner:#81A1C1,hl:#81A1C1,fg:#FFFFFF,header:#81A1C1,info:#FFFFFF,pointer:#81A1C1,marker:#81A1C1,fg+:-1,preview-bg:-1,prompt:#FFFFFF,hl+:#81A1C1,gutter:-1'
 
+
 autoload -U colors && colors
-PS1='%B%F{cyan}%(4~|...|) %3~%F{cyan}$(branch=$(git_branch_name) && [ -n "$branch" ] && echo " %F{white}${bold_cyan}$branch${white}") %F{white}> %b%f%k'
 setopt prompt_subst
+
+# Optional: function to get git branch name
+git_branch_name() {
+  git rev-parse --abbrev-ref HEAD 2>/dev/null
+}
+
+bold_cyan="%B%F{cyan}"
+white="%F{white}"
+gray="%F{242}"
+
+reset="%f%b%k"
+
+PS1="${gray}󰣇  ${white}%1~"
+PS1+='$(branch=$(git_branch_name); [ -n "$branch" ] && { short_branch=${branch:0:12}; [ ${#branch} -gt 12 ] && short_branch="${short_branch}…"; echo " ${gray}  ${white}${short_branch}${gray}"; })'
+PS1+=" ${gray}${reset} "
